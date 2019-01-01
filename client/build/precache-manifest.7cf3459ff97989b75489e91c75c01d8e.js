f35374f367daa2340aa13768f99859efe40a483a
bryan-lee-git <bryanvinyl@gmail.com>
2019-01-01 11:41:10 -0700

    changed lock files for deploy to heroku, also changed .gitignore file

---
+++ client/build/precache-manifest.7cf3459ff97989b75489e91c75c01d8e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a24d4b08e9bd9bd064a7",
+    "url": "/static/css/main.dadf5cf6.chunk.css"
+  },
+  {
+    "revision": "a24d4b08e9bd9bd064a7",
+    "url": "/static/js/main.a24d4b08.chunk.js"
+  },
+  {
+    "revision": "ebf7a370b359fe1b5a48",
+    "url": "/static/js/1.ebf7a370.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d8be1a2403fbc3fb3f9750120079cc8",
+    "url": "/index.html"
+  }
+];
